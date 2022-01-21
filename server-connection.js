a49a954cf5c611350fae5bda66a61dea55f7c2f9
Rita05 <redunm@mail.ru>
2022-01-22 00:57:24 +0300

    added policy file and send message method

--- server-connection.js
+++ server-connection.js
@@ -4,6 +4,12 @@ const app = express();
 app.set('port', 8081);
 
 app.use(express.static('./static'))
+app.use(express.json())
+
+app.post('/contact', (request, response) => {
+    response.send(JSON.stringify(request.body))
+})
+
 
 app.listen(8081)
 
--- static/scripts/header.js
+++ static/scripts/header.js
@@ -1,16 +1,46 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-const hamburgerElem=document.querySelector('.hamburger-menu')
-      menuElem=document.querySelector('.menu')
-      closeElem=document.querySelector('.menu__close')
+    let hamburgerElem = document.querySelector('.hamburger-menu')
+    let menuElem = document.querySelector('.menu')
+    let closeElem = document.querySelector('.menu__close')
+    let userName = document.querySelector('#name').value
+    let userEmail = document.querySelector('#email').value
+    let btnElem = document.querySelector('#btn__send-message')
 
-hamburgerElem.addEventListener('click', ()=>{
-    menuElem.classList.add('active') 
-})
+    async function sendMessage() {
 
-closeElem.addEventListener('click', ()=>{
-    menuElem.classList.remove('active')
-})
+        let userData = {
+            user: userName,
+            Email: userEmail
+        }
+
+        let response = await fetch('http://localhost:8081/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(userData)
+        })
+        let result = await response.json()
+        console.log(result.user)
+    }
+
+
+
+    hamburgerElem.addEventListener('click', () => {
+        menuElem.classList.add('active')
+    })
+
+    closeElem.addEventListener('click', () => {
+        menuElem.classList.remove('active')
+    })
+
+    btnElem.addEventListener('click', () => {
+        sendMessage()
+    })
+
+
+
 
 })
 
